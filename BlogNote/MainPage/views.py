5f68d854e03db3b7d1a2eb00944d4f287a8f146a
YuChernyshov <yuchernyshov@usurt.ru>
2020-02-26 12:36:03 +0500

    Pagination was added

--- BlogNote/MainPage/views.py
+++ BlogNote/MainPage/views.py
@@ -5,13 +5,26 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 from BlogNote.MainPage.models import model_Note
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def view_NotesList(request):
     notes = model_Note.objects.all()
-    t = loader.get_template("page_NotesList.html")
-    c = {"notes" : notes}
-    return HttpResponse(t.render(c))
+    paginator = Paginator(notes, 3) # 3 articles on page
+    page = request.GET.get('page')
+    try:
+        notes = paginator.page(page)
+    except PageNotAnInteger:
+        # ≈сли страница не €вл€етс€ целым числом, возвращаем первую страницу.
+        notes = paginator.page(1)
+    except EmptyPage:
+        # ≈сли номер страницы больше, чем общее количество страниц, возвращаем последнюю.
+        notes = paginator.page(paginator.num_pages)
+    return render(request,'page_NotesList.html', {'page': page, 'notes': notes})
+
+#    notes = model_Note.objects.all()
+#    t = loader.get_template("page_NotesList.html")
+#    c = {"notes" : notes}
+#    return HttpResponse(t.render(c))
 
 def view_NoteDetails(request, y, m, d, s):
     note = model_Note.objects.get(slug=s, status='published', publish__year=y, publish__month=m, publish__day=d)
