24a8653a263f413733fe86c46a113f6bbdda9589
YuChernyshov <yuchernyshov@usurt.ru>
2020-02-28 12:38:53 +0500

    Version succsessfully deployed on heroku

--- BlogNote/settings.py
+++ BlogNote/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'dm_879u$j(&8&-fqg@ojz$=ta4c@qq
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 # DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['lit-fortress-67024.herokuapp.com','127.0.0.1']
 # For example: 
