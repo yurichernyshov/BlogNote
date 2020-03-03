28db12ec7aaf5bb2967e6a1ef2052c32263ce0b6
YuChernyshov <yuchernyshov@usurt.ru>
2020-03-03 11:24:05 +0500

    MainPage name changed to Notes in setting.py

--- BlogNote/settings.py
+++ BlogNote/settings.py
@@ -96,7 +96,7 @@ DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
-LOGIN_REDIRECT_URL = 'MainPage:test'
+LOGIN_REDIRECT_URL = 'Notes:test'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
 
